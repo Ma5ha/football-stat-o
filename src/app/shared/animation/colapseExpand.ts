6e706f74099c5cb5de17b606eec67a7a6e35c1d3
Mladen <blagmash@gmail.com>
2020-06-27 21:49:25 +0200

    colapseExpand animation

---
+++ src/app/shared/animation/colapseExpand.ts
@@ -0,0 +1,48 @@
+import { AnimationTriggerMetadata, trigger, state, style, transition, animate } from '@angular/animations';
+
+export function expandColapse(h): AnimationTriggerMetadata {
+    return trigger('expandColapse',
+        [
+            state('expand',
+                style(
+                    {
+                        ...h,
+                        position: 'fixed',
+                        top: '0',
+                        width: '100%',
+                    },
+
+
+
+                ),
+                {
+                    params: {
+                        height: '2'
+                    }
+                }
+
+
+            ),
+            state('colapse',
+                style({
+                    height: '100px',
+                    backgroundColor: 'white',
+                    borderStyle: 'solid',
+                    position: 'fixed',
+                    top: '0',
+                    padding: '50px',
+                    width: '100%',
+                    display: 'none'
+
+
+                })
+            ),
+            transition('expand => colapse', [animate("1s ease-in")]),
+            transition('colapse => expand', [animate('1s ease-out')])
+
+
+        ]
+    )
+
+
+}
