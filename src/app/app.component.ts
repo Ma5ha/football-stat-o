114adbc028132fc241faa1137314560ee3022449
Mladen <blagmash@gmail.com>
2020-06-27 11:13:18 +0200

    nav bolierplate for delete  help for writting animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeInOnEnterAnimation({ anchor: 'enter', duration: 1000, delay: 100, translate: '30px' }), fadeOutOnLeaveAnimation()]
 })
 export class AppComponent {
   title = 'footballStats';
+  s
+
+  scroll(event) {
+
+    this.s = event.pageY
+
+    // console.log(window.scrollY)
+  }
 }
