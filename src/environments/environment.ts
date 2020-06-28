a6dc51912669c1dc18cf3b246b70b9ec34ea49a7
Mladen <blagmash@gmail.com>
2020-06-28 15:09:28 +0200

    enviroment updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiBase: 'https://apiv2.apifootball.com/?action=',
+  apiKey: '&APIkey=fd67343efddc02187d86d13f23d14ce2b6f138e91d340a42d4093dddf6834d7d'
 };
 
 /*
@@ -14,3 +16,22 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// {
+//   "country_id": "41",
+//   "country_name": "England",
+//   "league_id": "148",
+//   "league_name": "Premier League"
+// },
+
+// https://apiv2.apifootball.com/?action=get_leagues&country_id=41&APIkey=fd67343efddc02187d86d13f23d14ce2b6f138e91d340a42d4093dddf6834d7d
+// https://apiv2.apifootball.com/?action=get_leagues&id=148&APIkey=fd67343efddc02187d86d13f23d14ce2b6f138e91d340a42d4093dddf6834d7d
+
+
+// https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=fd67343efddc02187d86d13f23d14ce2b6f138e91d340a42d4093dddf6834d7d
+// https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=fd67343efddc02187d86d13f23d14ce2b6f138e91d340a42d4093dddf6834d7d
+
+
+// https://apiv2.apifootball.com/?action=get_events&fleague_id=148&APIkey=fd67343efddc02187d86d13f23d14ce2b6f138e91d340a42d4093dddf6834d7d
+
+
+// https://apiv2.apifootball.com/?action=get_events&from=2020-06-26&to=2020-06-27&league_id=148&APIkey=fd67343efddc02187d86d13f23d14ce2b6f138e91d340a42d4093dddf6834d7d
