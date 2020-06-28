135e330b848bb91b185a927be2a3c3d214694e80
Mladen <blagmash@gmail.com>
2020-06-28 08:38:02 +0200

    app component cleared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,7 @@ import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animati
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [fadeInOnEnterAnimation({ anchor: 'enter', duration: 1000, delay: 100, translate: '30px' }), fadeOutOnLeaveAnimation()]
 })
 export class AppComponent {
-  title = 'footballStats';
-  s
 
-  scroll(event) {
-
-    this.s = event.pageY
-
-    // console.log(window.scrollY)
-  }
 }
