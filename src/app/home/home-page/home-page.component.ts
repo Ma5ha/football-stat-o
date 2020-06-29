0e91a3394f58bb668de6e8f6c134f6e5a98f2143
Mladen <blagmash@gmail.com>
2020-06-29 22:51:51 +0200

    more animation

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { rotateInAnimation, slideInDownOnEnterAnimation, flipInYOnEnterAnimation } from 'angular-animations';
-import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation, flipOnEnterAnimation, slideInLeftOnEnterAnimation } from 'angular-animations';
+import { rotateInAnimation, slideInDownOnEnterAnimation, flipInYOnEnterAnimation, slideInUpAnimation, fadeOutAnimation } from 'angular-animations';
+import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation, flipOnEnterAnimation, slideInLeftOnEnterAnimation, } from 'angular-animations';
 import { StandigsService } from '../../shared/services/standigs.service'
 
 
 @Component({
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  animations: [flipInYOnEnterAnimation(), slideInLeftOnEnterAnimation(), flipOnEnterAnimation(), slideInDownOnEnterAnimation(), rotateInAnimation({ anchor: 'rotate', duration: 10000, delay: 1000 }), fadeInOnEnterAnimation({ anchor: 'enter', duration: 1000, delay: 1000 })]
+  animations: [fadeOutAnimation(), flipInYOnEnterAnimation(), slideInLeftOnEnterAnimation(), flipOnEnterAnimation(), slideInDownOnEnterAnimation(), rotateInAnimation({ anchor: 'rotate', duration: 10000, delay: 1000 }), fadeInOnEnterAnimation({ anchor: 'enter', duration: 1000, delay: 1000 })]
 })
 export class HomePageComponent implements OnInit {
 
@@ -24,9 +24,13 @@ export class HomePageComponent implements OnInit {
     )
   }
 
+  team
 
-  t = false
+  r = false
 
+  clickHandler(p) {
 
+    this.team = p.team_id
+  }
 
 }
