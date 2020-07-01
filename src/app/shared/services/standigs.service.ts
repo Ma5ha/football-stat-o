49137cd6e0a9faf3949d1347b885f4a83ca655f9
Mladen <blagmash@gmail.com>
2020-07-01 14:36:55 +0200

    working getFixturesQuery

--- src/app/shared/services/standigs.service.ts
+++ src/app/shared/services/standigs.service.ts
@@ -4,6 +4,8 @@ import { environment } from '../../../environments/environment'
 import { Observable } from 'rxjs';
 import { Club } from '../types/club';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,11 +13,21 @@ import { Club } from '../types/club';
 
 export class StandigsService {
 
+
+
+
+
+
   constructor(private standingsService: HttpClient) { }
 
   getStandings(): Observable<Club[]> {
     return this.standingsService.get<Club[]>(`${environment.apiBase}get_standings&league_id=148${environment.apiKey}`)
   }
+  getFixtures(from: string, to: number): Observable<object> {
 
+
+    let get_events = `get_events&from=${from}&to=${to}&league_id`
+    return this.standingsService.get(`${environment.apiBase}${get_events}&league_id=148${environment.apiKey}`)
+  }
 
 }
