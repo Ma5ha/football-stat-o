cf388f8556f129f043c3e2534c1fb5c55593e9fa
Mladen <blagmash@gmail.com>
2020-07-02 15:00:34 +0200

    fixtures table generaded

---
+++ src/app/table/table-page/fixtures-table/fixtures-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fixtures-table',
+  templateUrl: './fixtures-table.component.html',
+  styleUrls: ['./fixtures-table.component.scss']
+})
+export class FixturesTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TableRoutingModule } from './table-routing.module';
 import { TablePageComponent } from './table-page/table-page.component';
+import { FixturesTableComponent } from './table-page/fixtures-table/fixtures-table.component';
 
 
 @NgModule({
-  declarations: [TablePageComponent],
+  declarations: [TablePageComponent, FixturesTableComponent],
   imports: [
     CommonModule,
     TableRoutingModule
