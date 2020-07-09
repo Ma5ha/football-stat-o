fcb8d6045193a316c4fa706fa6a2869b6a9d8ae2
Mladen <blagmash@gmail.com>
2020-07-09 17:07:27 +0200

    production envirome changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  apiBase: 'https://apiv2.apifootball.com/?action=',
+  apiKey: '&APIkey=fd67343efddc02187d86d13f23d14ce2b6f138e91d340a42d4093dddf6834d7d'
 };
