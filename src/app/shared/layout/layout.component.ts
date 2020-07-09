e2e2f6a9873056d11ed100de524e158553ff5c87
Mladen <blagmash@gmail.com>
2020-07-09 16:17:22 +0200

    some changes all good

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -15,7 +15,7 @@ export class LayoutComponent implements OnInit {
   @HostListener('window:scroll')
   scrollHandler() {
 
-    if (window.scrollY < 1) return false
+    if (window.scrollY < 1) return true
     return false
   }
 
