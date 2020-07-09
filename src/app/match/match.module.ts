5a166e199704ed0063aacd5f5b4fe606de45e793
Mladen <blagmash@gmail.com>
2020-07-09 15:48:24 +0200

    tooltip module added

--- src/app/match/match.module.ts
+++ src/app/match/match.module.ts
@@ -7,7 +7,7 @@ import { SharedModule } from '../shared/shared.module';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { BarChartComponent } from './mach-page/bar-chart/bar-chart.component';
 import { TacticBoardComponent } from './mach-page/tactic-board/tactic-board.component';
-
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [MachPageComponent, BarChartComponent, TacticBoardComponent],
   imports: [
@@ -16,6 +16,7 @@ import { TacticBoardComponent } from './mach-page/tactic-board/tactic-board.comp
     MatchRoutingModule,
     NgxChartsModule,
     SharedModule,
+    NgbTooltipModule
   ]
 })
 export class MatchModule { }
